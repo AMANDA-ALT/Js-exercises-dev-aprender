b9f74e897c1fcba373a8fba53f6d4a08c96c06cb
Amanda <amandabaratadev@gmail.com>
2023-01-19 13:48:40 +0000

    first exercise max between 2 values

---
+++ index.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line max-len
+/* First mini project - máximo entre 2 valores: Escreva uma função que usa 2 números e retorna o maior entre eles */
+
+let maxValue = max(5,9);
+console.log(maxValue);
+
+function max(num1, num2) {
+if(num1 > num2)
+    return num1;
+else 
+    return num2;
+}
+
+// will log 9 in this case
+
+//Usando operador ternário:
+
+/*let maxValue = max(5,9);
+console.log(maxValue);
+function max(num1, num2) {
+   return num1 > num2 ? num1 : num2;*/
+}
+
